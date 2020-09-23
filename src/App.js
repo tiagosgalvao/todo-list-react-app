d7985bbc52be181b691ec91b01dcd3bb505f7701
tiagogalvao <tiago.galvao@topia.com>
2020-09-24 01:05:00 +0300

    todo management page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import FilterProvider from "./state/filter/Provider";
 import TodosProvider from "./state/todos/Provider";
+import TodoApp from "./pages/TodoApp/TodoApp";
 
 function App() {
   return (
     <TodosProvider>
       <FilterProvider>
-        <h1>Hello World!</h1>
+        <TodoApp />
       </FilterProvider>
     </TodosProvider>
   );
---
+++ src/pages/TodoApp/TodoApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function TodoApp() {
+  return (
+    <>
+      <header>Todo Creator</header>
+      <main>TodoList</main>
+      <footer>TodoFilter</footer>
+    </>
+  );
+}
+
+export default TodoApp;
