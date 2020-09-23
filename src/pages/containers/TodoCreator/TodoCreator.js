8774ee46a395fe3d95e25f4fcac742fe1498cbd6
tiagogalvao <tiago.galvao@topia.com>
2020-09-24 02:24:36 +0300

    css form create task

--- src/pages/containers/TodoCreator/TodoCreator.js
+++ src/pages/containers/TodoCreator/TodoCreator.js
@@ -1,30 +1,43 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { useFormik } from "formik";
 import TodosContext from "../../../state/todos/Context";
 import * as todosActions from "../../../state/todos/actions";
+import * as yup from "yup";
+import styles from "./TodoCreator.module.css";
 
 function TodoCreator() {
-  const { todos, todosDispatch } = useContext(TodosContext);
-  useEffect(() => {
-    console.log(todos);
-  }, [todos]);
-  const formik = useFormik({
+  const { todosDispatch } = useContext(TodosContext);
+  const { getFieldProps, touched, errors, isValid, handleSubmit } = useFormik({
     initialValues: {
       title: "",
     },
-    onSubmit: (values) => {
+    validationSchema: yup.object({
+      title: yup.string().required("Task title is mandatory"),
+    }),
+    onSubmit: (values, formikBag) => {
       todosDispatch(todosActions.addTodo(values.title));
+      formikBag.setFieldValue("title", "", false);
     },
   });
+  const inputTitle = useRef(null);
+  useEffect(() => {
+    inputTitle.current.focus();
+  }, []);
   return (
-    <form onSubmit={formik.handleSubmit}>
+    <form className={styles.container} onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="New task"
         autoComplete="off"
-        {...formik.getFieldProps("title")}
+        ref={inputTitle}
+        {...getFieldProps("title")}
       ></input>
-      <button type="submit">Add task</button>
+      {touched.title && errors.title ? (
+        <small className={styles.error}>{errors.title}</small>
+      ) : null}
+      <button type="submit" disabled={!isValid}>
+        Add task
+      </button>
     </form>
   );
 }
