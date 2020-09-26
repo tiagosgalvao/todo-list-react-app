b1ce089573fdc2446bd22d13ba7bb1876cf42e9c
tiagogalvao <tiago.galvao@topia.com>
2020-09-26 14:41:54 +0300

    handleChange - handling the state for the checkboxes

--- src/pages/containers/TodoList/TodoList.js
+++ src/pages/containers/TodoList/TodoList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useCallback } from "react";
+import React, { useContext, useCallback, useEffect } from "react";
 import TodoContext from "../../../state/todos/Context";
 import styles from "./TodoList.module.css";
 import TodoItem from "./components/TodoItem/TodoItem";
@@ -6,20 +6,29 @@ import * as todosActions from "../../../state/todos/actions";
 
 function TodoList() {
   const { todos, todosDispatch } = useContext(TodoContext);
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
   const handleDelete = useCallback(
     (id) => {
       todosDispatch(todosActions.removeTodo(id));
     },
     [todosDispatch]
   );
+  const handleStatusUpdate = useCallback((id, completed) => {
+    todosDispatch(todosActions.toggleTodoStatus(id, completed));
+  }, []);
   return (
     <div className={styles.container}>
       <ul>
         {todos.map((todo) => {
           return (
             <TodoItem
+              id={todo.id}
               key={todo.id}
               title={todo.title}
+              completed={todo.completed}
+              onStatusUpdate={handleStatusUpdate}
               onDelete={() => {
                 handleDelete(todo.id);
               }}
--- src/pages/containers/TodoList/components/TodoItem/TodoItem.js
+++ src/pages/containers/TodoList/components/TodoItem/TodoItem.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
-function TodoItem({ title, onDelete }) {
+function TodoItem({ id, title, completed, onStatusUpdate, onDelete }) {
+  const [isChecked, setIsChecked] = useState(completed);
+  const handleChange = useCallback((evt) => {
+    setIsChecked(evt.target.checked);
+  }, []);
+  useEffect(() => {
+    onStatusUpdate(id, isChecked);
+  }, [id, isChecked, onStatusUpdate]);
   return (
     <li>
       <span>{title}</span>
       <button onClick={onDelete}>Deletar</button>
+      <input type="checkbox" value={isChecked} onChange={handleChange} />
     </li>
   );
 }
